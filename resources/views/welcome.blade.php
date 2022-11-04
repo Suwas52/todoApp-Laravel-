2a19902a17feb8eecd1fe9c7b4451fa007a4a85d
Suwas52 <suwasdanuwar18@gmail.com>
2022-11-04 15:55:54 +0545

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: skyblue;
+            height:100vh;
+        }
+
+        h1 {
+            text-align:center;
+            color:red;
+        }
+        ul{
+            list-style: none;
+        }
+    </style>
+</head>
+<body style="background: skyblue; height:100vh">
+    
+    <h1>Todo Application</h1>
+
+    <form action="{{route('saveTodo')}}" method="post">
+        @csrf
+        <input type="text" name="todo">
+        <button type="submit">Save</button>
+    </form>
+</body>
+</html>
